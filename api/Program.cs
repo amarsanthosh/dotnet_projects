5bc50dedbfe8b136e3d7cfe51e4ad36ea531916e
amarsanthosh <amarsanthoshsanthosh@gmail.com>
2025-07-31 12:02:48 +0530

    Added identity and Authentication step 1&2

--- api/Program.cs
+++ api/Program.cs
@@ -1,7 +1,13 @@
 using api.Data;
 using api.Interface;
+using api.Models;
 using api.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.CodeAnalysis.Options;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +18,39 @@ builder.Services.AddDbContext<ApplicationDbContext>(Options =>
 {
     Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddIdentity<IdentityUser, AppUser>(Options =>
+{
+    Options.Password.RequireDigit = true;
+    Options.Password.RequiredLength = 8;
+    Options.Password.RequireLowercase = true;
+    Options.Password.RequireUppercase = true;
+}).AddEntityFrameworkStores<ApplicationDbContext>();
+
+var jwtSettings = builder.Configuration.GetSection("Jwt"); 
+builder.Services.AddAuthentication(Options =>
+{
+    Options.DefaultAuthenticateScheme =
+    Options.DefaultChallengeScheme =
+    Options.DefaultForbidScheme =
+    Options.DefaultScheme =
+    Options.DefaultSignInScheme =
+    Options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(Options =>
+{
+    Options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidateLifetime = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            System.Text.Encoding.UTF8.GetBytes(jwtSettings["Signingkey"]!)
+        )
+    }; 
+} ); 
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddControllers();
