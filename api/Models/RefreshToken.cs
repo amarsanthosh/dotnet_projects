ef3f87f595b9e3c80bfd7b9648cafb72de6a9e1a
amarsanthosh <amarsanthoshsanthosh@gmail.com>
2025-08-01 23:20:58 +0530

    Added Refresh token model

---
+++ api/Models/RefreshToken.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class RefreshToken
+    {
+        public int Id { get; set; }
+        public string Token { get; set; } = string.Empty;
+        public DateTime Expires { get; set; }
+        public bool IsExpired => DateTime.UtcNow >= Expires;
+        public DateTime Created { get; set; }
+        public string CreatedByIp { get; set; } = string.Empty;
+        public DateTime? Revoked { get; set; }
+        public string RevokedByIp { get; set; } = string.Empty;
+        public bool IsActive => Revoked == null && !IsExpired; 
+
+    }
+}
